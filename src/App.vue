d9f15a7dd1bfac9e8088309ed2842432934e177f
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-07-09 23:39:07 +0700

    app

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ const mobileMenuOpen = ref(false)
       <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div class="flex lg:flex-1">
           <a href="#" class="-m-1.5 p-1.5">
-            <span class="sr-only">Your Company</span>
+            <span class="sr-only">Your Companya</span>
             <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
           </a>
         </div>
