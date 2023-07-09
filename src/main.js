3e3268918f899a7aed9b4543a19f682e1da40b95
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-07-09 23:47:02 +0700

    router

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import Vue from "vue";
-import router from "./router";
+import router from "./routers";
 
 Vue.config.productionTip = false;
 
---
+++ src/routers/index.js
@@ -0,0 +1,45 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Login from "../components/Login.vue";
+import Register from "../components/Register.vue";
+import Dashboard from "../components/Dashboard.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    component: Dashboard,
+    meta: { requiresAuth: true }, // Menandai bahwa halaman ini membutuhkan autentikasi
+  },
+  { path: "*", redirect: "/login" }, // Mengarahkan halaman tidak ditemukan ke halaman login
+];
+
+const router = new VueRouter({
+  mode: "history",
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+  if (to.matched.some((record) => record.meta.requiresAuth) && !isLoggedIn) {
+    // Jika pengguna belum login dan mencoba mengakses halaman yang membutuhkan autentikasi,
+    // arahkan mereka ke halaman login
+    next("/login");
+  } else {
+    next();
+  }
+});
+
+export default router;
