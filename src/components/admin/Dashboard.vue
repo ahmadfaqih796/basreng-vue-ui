ab62418cf87b90c23d7660a6ae44e86e5768d4be
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-07-09 23:57:18 +0700

    dashboard

---
+++ src/components/admin/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+   <div>
+     <h1>Welcome to the Dashboard!</h1>
+     <!-- Konten dashboard lainnya -->
+   </div>
+ </template>
+
+<script>
+export default {
+  created() {
+    // Periksa status login saat komponen dibuat
+    const isLoggedIn = localStorage.getItem('isLoggedIn');
+    if (!isLoggedIn) {
+      // Jika tidak ada status login, arahkan pengguna ke halaman login
+      this.$router.push('/login');
+    }
+  }
+}
+</script>
